e74a1cfe0e8a304d420cadcec516e74e08bc2d13
Debashis Nandi <debashisnandi576@gmail.com>
2026-06-15 00:46:30 +0530

    feat: POTD 2026-06-14 solved

---
+++ src/2100-2199/2130.cpp
@@ -0,0 +1,123 @@
+/*
+Tags
+level-medium
+linked-list, two-pointers, stack
+
+Problem Description
+2130. Maximum Twin Sum of a Linked List
+In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
+The twin sum is defined as the sum of a node and its twin.
+Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+
+Example 1:
+Input: head = [5,4,2,1]
+Output: 6
+Explanation:
+Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+There are no other nodes with twins in the linked list.
+Thus, the maximum twin sum of the linked list is 6.
+
+Example 2:
+Input: head = [4,2,2,3]
+Output: 7
+Explanation:
+The nodes with twins present in this linked list are:
+- Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+- Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+Thus, the maximum twin sum of the linked list is max(7, 4) = 7.
+
+Example 3:
+Input: head = [1,100000]
+Output: 100001
+Explanation:
+There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001.
+
+Constraints:
+The number of nodes in the list is an even integer in the range [2, 10^5].
+1 <= Node.val <= 10^5
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <limits>
+#include <iomanip>
+#include <functional>
+#include <cstring>
+#include <climits>
+#include "../../core/core.h"
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int pairSum(ListNode* head)
+    {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        stack<int> st;
+        while (fast != nullptr)
+        {
+            fast = fast->next->next;
+            st.push(slow->val);
+            slow = slow->next;
+        }
+        int maxPairSum = 0;
+        while (slow != nullptr)
+        {
+            int val = st.top();
+            st.pop();
+            maxPairSum = max(maxPairSum, val + slow->val);
+            slow = slow->next;
+        }
+
+        return maxPairSum;
+    }
+};
+
+int main()
+{
+    int n;
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+    cin >> n;
+    int x;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        if (head == nullptr)
+        {
+            head = new ListNode(x);
+            tail = head;
+        }
+        else
+        {
+            tail->next = new ListNode(x);
+        }
+    }
+    Solution sol;
+    int result = sol.pairSum(head);
+    cout << result << "\n";
+	return 0;
+}
